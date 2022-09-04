b67978ce7c12735c41fb73c7490476e04627de2d
Erikson <chris.s.erickson@gmail.com>
2022-09-04 15:55:33 -0400

    bcrypt

--- models/user.js
+++ models/user.js
@@ -2,6 +2,9 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
+// encryption npm install bcrypt
+const bcrypt = require("bcrypt");
+
 // create our User model
 class User extends Model {}
 
@@ -48,8 +51,14 @@ User.init(
     },
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
