f404670f5e4bfabfd86f6df24995b3ed0ff64b07
Erikson <chris.s.erickson@gmail.com>
2022-09-05 12:41:26 -0400

    voter routes

--- models/Post.js
+++ models/Post.js
@@ -1,46 +1,65 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
-
-// Create our Post Model
-class Post extends Model {}
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// create our Post model
+class Post extends Model {
+  static upvote(body, models) {
+    return models.Vote.create({
+      user_id: body.user_id,
+      post_id: body.post_id
+    }).then(() => {
+      return Post.findOne({
+        where: {
+          id: body.post_id
+        },
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [
+            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+            'vote_count'
+          ]
+        ]
+      });
+    });
+  }
+}
 
 // create fields/columns for Post model
 Post.init(
-    // first parameter sets schema
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
     title: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     post_url: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isURL: true,
-      },
+        isURL: true
+      }
     },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "user",
-        key: "id",
-      },
-    },
-    },
-    //second parameter configures metadata including naming conventions
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: "post",
+    modelName: 'post'
   }
 );
 
-// export expression
 module.exports = Post;
