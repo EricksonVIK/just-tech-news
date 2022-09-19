619d9f85361d89527f17cdc220a506ebb8a849b9
Erikson <chris.s.erickson@gmail.com>
2022-09-18 23:21:09 -0400

    added log in and out

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -43,4 +43,13 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+  console.log(req.session);
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
 module.exports = router;
